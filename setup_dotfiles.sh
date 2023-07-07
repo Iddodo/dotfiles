f07fe1cd140215c890bcdedd65c1f1da026af2b9
Iddodo <m.ido@campus.technion.ac.il>
2023-07-07 09:51:23 +0300

    Soft link to .macos file

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -10,6 +10,7 @@ mkdir -p $HOME/.config
 ln -s $DOTFILE_LOC/.doom.d $HOME/.doom.d
 ln -s $DOTFILE_LOC/.zshrc $HOME/.zshrc
 ln -s $DOTFILE_LOC/.tmux.conf $HOME/.tmux.conf
+ln -s $DOTFILE_LOC/macOS-configuration/.macos $HOME/.macos
 
 cd $DOTFILE_LOC/.config
 for file in *; do
